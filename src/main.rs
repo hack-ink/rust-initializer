37c6f969f701433c5cc13a7f2e2e8bd12c339595
Xavier Lau <xavier@inv.cafe>
2024-04-04 18:35:04 +0800

    Add cli module

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
+mod cli;
+use cli::Cli;
+
 // crates.io
 use anyhow::Result;
+use clap::Parser;
 
 fn main() -> Result<()> {
 	color_eyre::install().unwrap();
 	tracing_subscriber::fmt::init();
+
+	Cli::parse().run()?;
 
 	Ok(())
 }
